3f6c1e55360f0a5d6a503fe5ffa5775c1244a709
Dmitrii G <18750503+goriunov@users.noreply.github.com>
2024-04-26 19:02:13 +1200

    Rename to handler

--- handler_builder.go
+++ handler_builder.go
@@ -2,7 +2,7 @@ package gws
 
 // very simple handler builder to support event registration without creating separate struct for handling the events
 
-type HandlerBuilder struct {
+type Handler struct {
 	onPongHandler    func(socket *Conn, message []byte)
 	onPingHandler    func(socket *Conn, message []byte)
 	onCloseHandler   func(socket *Conn, err error)
@@ -10,8 +10,8 @@ type HandlerBuilder struct {
 	onMessageHandler func(socket *Conn, message *Message)
 }
 
-func NewHandler() *HandlerBuilder {
-	return &HandlerBuilder{
+func NewHandler() *Handler {
+	return &Handler{
 		onPongHandler:    func(_ *Conn, _ []byte) {},
 		onPingHandler:    func(_ *Conn, _ []byte) {},
 		onCloseHandler:   func(_ *Conn, _ error) {},
@@ -21,41 +21,41 @@ func NewHandler() *HandlerBuilder {
 }
 
 // attach on pong handler
-func (c *HandlerBuilder) OnPong(socket *Conn, message []byte) {
+func (c *Handler) OnPong(socket *Conn, message []byte) {
 	c.onPongHandler(socket, message)
 }
-func (c *HandlerBuilder) SetOnPongHandler(h func(socket *Conn, message []byte)) {
+func (c *Handler) SetOnPongHandler(h func(socket *Conn, message []byte)) {
 	c.onPongHandler = h
 }
 
 // attach on ping handler
-func (c *HandlerBuilder) OnPing(socket *Conn, message []byte) {
+func (c *Handler) OnPing(socket *Conn, message []byte) {
 	c.onPingHandler(socket, message)
 }
-func (c *HandlerBuilder) SetOnPingHandler(h func(socket *Conn, message []byte)) {
+func (c *Handler) SetOnPingHandler(h func(socket *Conn, message []byte)) {
 	c.onPingHandler = h
 }
 
 // attach on close handler
-func (c *HandlerBuilder) OnClose(socket *Conn, err error) {
+func (c *Handler) OnClose(socket *Conn, err error) {
 	c.onCloseHandler(socket, err)
 }
-func (c *HandlerBuilder) SetOnCloseHandler(h func(socket *Conn, err error)) {
+func (c *Handler) SetOnCloseHandler(h func(socket *Conn, err error)) {
 	c.onCloseHandler = h
 }
 
 // attach on open handler
-func (c *HandlerBuilder) OnOpen(socket *Conn) {
+func (c *Handler) OnOpen(socket *Conn) {
 	c.onOpenHandler(socket)
 }
-func (c *HandlerBuilder) SetOnOpenHandler(h func(socket *Conn)) {
+func (c *Handler) SetOnOpenHandler(h func(socket *Conn)) {
 	c.onOpenHandler = h
 }
 
 // attach on message handler
-func (c *HandlerBuilder) OnMessage(socket *Conn, message *Message) {
+func (c *Handler) OnMessage(socket *Conn, message *Message) {
 	c.onMessageHandler(socket, message)
 }
-func (c *HandlerBuilder) SetOnMessageHandler(h func(socket *Conn, message *Message)) {
+func (c *Handler) SetOnMessageHandler(h func(socket *Conn, message *Message)) {
 	c.onMessageHandler = h
 }
